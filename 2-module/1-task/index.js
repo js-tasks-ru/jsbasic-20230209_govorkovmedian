07cc2b922248995f81b630aee6d2ce0fbaac1350
Ilya Govorkov <i.govorkov@qiwi.com>
2023-02-16 18:01:11 +0200

    2.1 done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  
+  for(let i in salaries){
+      if(typeof salaries[i] === "number" && ![NaN, Infinity, -Infinity].includes(salaries[i]))
+      {
+          sum += salaries[i];
+      }
+  }
+  
+  return sum;
 }
