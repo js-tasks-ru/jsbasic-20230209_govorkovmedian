4b1e3bae68f7eb3ee024531cd3a3e248a35cbd91
Ilya Govorkov <i.govorkov@qiwi.com>
2023-02-28 09:25:16 +0200

    Fix the complexity. KISS

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,16 +1,6 @@
 function filterRange(arr, a, b) {
   
-  // if we know for sure NaN can not occur in our array
-  return arr.map((elem)=> (elem >= a) && (elem <= b) ? elem : NaN).filter((e)=> !isNaN(e));
+  return arr.filter(elem => (elem >= a) && (elem <= b));
 
-  /* or what is the same
 
-  let new_arr = [];
-  for(let i=0; i<arr.length; i++)
-  {
-    if((arr[i] >= a) && (arr[i] <= b))
-      new_arr.push(arr[i]);
-  }
-  return new_arr;
-  */
 }
