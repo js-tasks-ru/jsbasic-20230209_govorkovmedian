956630c7341bef18998eb41059fafe27c3edee93
Ilya Govorkov <i.govorkov@qiwi.com>
2023-03-06 16:56:18 +0200

    4.1 done

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
   // ваш код...
+
+  let ul = document.createElement('ul');
+
+  ul.append(...friends.map(obj => {let li = document.createElement('li'); li.innerText = `${obj.firstName} ${obj.lastName}`; return li;}));
+
+  return ul;
 }
