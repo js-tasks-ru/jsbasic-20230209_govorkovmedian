55c93aa91fe2adea29978256fb45952dd2e8b8c3
Ilya Govorkov <i.govorkov@qiwi.com>
2023-03-09 16:58:37 +0200

    5.{1,2} done\nSimple element (un-)hiding

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,6 @@
 function hideSelf() {
   // ваш код...
+
+  document.querySelector('.hide-self-button').onclick = function(evt){this.hidden = this.hidden ? false : true;};
+
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
   // ваш код...
+
+  document.querySelector('.toggle-text-button').onclick = function(evt){
+    let target = document.querySelector('#text');
+    target.hidden = !target.hidden;
+  };
+
 }
